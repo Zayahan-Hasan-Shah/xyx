070de17c5b33061fb31c527be64813fe1c67eed0
Zayahan-Hasan-Shah <113588072+Zayahan-Hasan-Shah@users.noreply.github.com>
2023-11-08 15:15:33 +0500

    Create priority.py

---
+++ priority.py
@@ -0,0 +1,24 @@
+#Priority formula
+import math
+
+
+priority = []
+A = 55
+M = 1994
+Zo = 10112166
+C = 9
+
+for i in range(11):
+    LCG = ((A * Zo) + C) % M
+    X = LCG / M
+    Zo = LCG
+    Y = ((3 - 1) * X) + 1
+    if Y - math.floor(Y) >= 0.5:
+        Y = math.ceil(Y)
+        priority.append(Y)
+    else:
+        Y = math.floor(Y)
+        priority.append(Y)
+
+for i in range(len(priority)):
+    print(priority[i])
